88665d6506d4cdaedb91efbee4d9e3fac1e1dcfd
Wood Wu <wood.wu@elephanttalk.com>
2016-04-23 21:57:16 +0800

    SoapHeader authentication

---
+++ daan.webservice.phyReportSystem/Framework/Authenticaition/AuthenticaitionServiceImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Daan.Authority.Handler;
+using log4net;
+
+namespace daan.webservice.phyReportSystem.Framework.Authenticaition
+{
+    public class AuthenticaitionServiceImpl
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public bool Authenticate(UserCredential userCredential)
+        {
+            SecurityHandler security = SecurityHandler.Login(userCredential.Username, userCredential.Password);
+            if (security.LoginResult.SystemCode != null)
+            {
+                Log.Info("Authenticate OK");
+
+                // Do something, for example load user data in the context
+                return true;
+            }
+            else
+            {
+                Log.Info("Authenticate fail.");
+                return false;
+            }
+        }
+    }
+}
---
+++ daan.webservice.phyReportSystem/Framework/Authenticaition/MockAuthenticaitionServiceImpl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+
+namespace daan.webservice.phyReportSystem.Framework.Authenticaition
+{
+    public class MockAuthenticaitionServiceImpl
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public bool Authenticate(UserCredential userCredential)
+        {
+            Log.Info("Authenticate OK");
+            return true;
+        }
+    }
+}
